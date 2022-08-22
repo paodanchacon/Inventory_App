297727e0db245edba2df7262c1dd6f22d6ba91f1
Daniela Chambilla <63526399+paodanchacon@users.noreply.github.com>
2022-08-22 08:16:10 -0400

    Presented header section for the app

--- main.py
+++ main.py
@@ -7,7 +7,14 @@ left_column, right_column = st.columns(2)
 
 with st.container():
     
-    st.subheader("Hi, I am Daniela Chambilla :yellow_heart:")
+    
     st.markdown("***INVENTORY APP***")
     st.title("Expected_Stock Vs Counted_Stock")
-    st.subheader("")
+    from PIL import Image
+    st.subheader("Presentation by: Daniela Chambilla :yellow_heart:")
+    image=Image.open("store3.png")
+    st.image(image, use_column_width=True)
+
+    st.write("How can you to verify the real amount of missing and suplus equipment in your wharehouse?")
+    st.write("The goal is to take action to reduce the quantity of missing equipment, and also to improve the control of the warehouse")
+
